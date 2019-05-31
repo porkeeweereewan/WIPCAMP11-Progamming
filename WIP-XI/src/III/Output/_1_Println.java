0798cbc6de9c7ee60d6f3890f02eb8126017fdab
porkeeweereewan <51141422+porkeeweereewan@users.noreply.github.com>
2019-05-31 08:44:56 +0700

    Add project

---
+++ WIP-XI/src/III/Output/_1_Println.java
@@ -0,0 +1,18 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Println {
+    public static void main(String[] args) {
+        String name = "pop";
+        System.out.println("hello word" + name);
+        
+        System.out.println("ถ้าเราเอา " + 1 + " ไปบวก " + 100 + " เราจะได้ " + 1+100);
+    }
+}
---
+++ WIP-XI/src/V/Selection/_3_Exercise.java
@@ -0,0 +1,29 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package V.Selection;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _3_Exercise {
+    public static void main(String[] args) {
+        int x = 9;
+        
+        if(x == 9){
+            
+            if(x < 6) {
+                x = 8;
+            } else {
+                x = 4;
+            }
+            
+        }else{x=8} 
+        
+        
+        
+        System.out.println(x);
+    }
+}
